29a12bad17972bb364268d7f6648889373a21440
Samet SAN <sametsan88@gmail.com>
2021-12-07 16:51:10 +0300

    added debug

--- uart-control.ino
+++ uart-control.ino
@@ -1,36 +1,43 @@
 #define USART_BAUDRATE 9600
 
+#define DEBUG
+
+#ifdef DEBUG
+#define debug(...) Serial.print(__VA_ARGS__)
+#define debugln(...) Serial.println(__VA_ARGS__)
+#else
+#define debugln(...)
+#define debug(...)
+#endif
+
 int timeout = 300;
 String command;
 bool is_active_echo = true;
 
 void command_parse() {
 
-  if (command.length() == 0)
-    return;
-
   if (!command.compareTo("start")) {
     is_active_echo = true;
-    Serial.println("Echo started");
+    debugln("Echo started");
   }
 
   if (!command.compareTo("stop")) {
     is_active_echo = false;
-    Serial.println("Echo Stopped");
+    debugln("Echo Stopped");
   }
 
   int index = command.indexOf('=');
 
   if (!command.substring(0, index).compareTo("ledon")) {
     timeout = command.substring(index + 1).toInt();
-    Serial.print("Led on time = ");
-    Serial.println(timeout);
+    debug("Led on time = ");
+    debugln(timeout);
   }
 
   if (!command.substring(0, index).compareTo("ledoff")) {
     timeout = 1000 - command.substring(index + 1).toInt();
-    Serial.print("Led off time = ");
-    Serial.println(1000 - timeout);
+    debug("Led off time = ");
+    debugln(1000 - timeout);
   }
 
   // Command buffer clear
@@ -59,6 +66,8 @@ void serialEvent() {
     command += ch;
   }
 
+  command_parse();
+
   if (is_active_echo)
     Serial.println(command.c_str());
 
@@ -67,7 +76,7 @@ void serialEvent() {
 
 void loop() {
 
-  command_parse();
+
 
   digitalWrite(2, LOW);
   delay(1000 - timeout);
